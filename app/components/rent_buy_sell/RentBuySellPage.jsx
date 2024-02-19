aa7bf3dc75e5111d6bdf061e7c7dcd1df842068a
msbhh-ms <learnwithmsbhh@gmail.com>
2024-02-19 22:13:53 +0600

    updated rent buy sell page

--- app/components/rent_buy_sell/RentBuySellPage.jsx
+++ app/components/rent_buy_sell/RentBuySellPage.jsx
@@ -2,7 +2,7 @@ import HeaderAndBody from "./components/HeaderAndBody";
 
 export default function RentBuySellPage({ defaultTab }) {
   return (
-    <div className="w-full max-w-[1280px] mx-auto px-5 md:px-8 py-[30px] md:py-[65px]">
+    <div className="w-full max-w-[1280px] mx-auto px-5 md:px-8 py-[25px] md:py-[35px]">
       <div>
         <HeaderAndBody defaultTab={defaultTab} />
       </div>
