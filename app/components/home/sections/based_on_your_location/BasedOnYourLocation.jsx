05db1da5720ed74217ef8107bdc885fe3a6b0b91
msbhh-ms <learnwithmsbhh@gmail.com>
2024-02-19 22:21:57 +0600

    updated based on your location page

--- app/components/home/sections/based_on_your_location/BasedOnYourLocation.jsx
+++ app/components/home/sections/based_on_your_location/BasedOnYourLocation.jsx
@@ -9,6 +9,16 @@ export const BasedOnYourLocation = () => (
     <div className="w-full max-w-[1280px] mx-auto px-4 md:px-8 ">
       <div className="grid grid-cols-12">
         <div className="col-span-12 pb-10">
+          <div className="grid gap-[18px] md:gap-[24px]  pb-[35px] md:pb-[75px]">
+            <div>
+              <h2 className="font-bold text-primary text-[30px] md:text-[40px] text-center">
+                Based on your location
+              </h2>
+            </div>
+            <div className="font-normal text-[14px] md:text-[16px] text-graylight text-center">
+              Some of our picked properties near you location.
+            </div>
+          </div>
           <HeaderAndBodySection />
         </div>
       </div>
