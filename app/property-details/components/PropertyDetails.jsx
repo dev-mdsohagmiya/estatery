b3d0a28658cbac68ac7f3a284bcf945c3321a809
msbhh-ms <learnwithmsbhh@gmail.com>
2024-02-20 23:47:06 +0600

    updated property details page

--- app/property-details/components/PropertyDetails.jsx
+++ app/property-details/components/PropertyDetails.jsx
@@ -1,16 +1,20 @@
 import BreadcrumbSec from "./breadcrumbs/BreadcrumbSec";
 import Header from "./header/Header";
+import PhotoFrame from "./photo-frame/PhotoFrame";
 
 export default function PropertyDetails() {
   return (
     <div className="py-6 md:py-8">
       <div className="w-full max-w-[1280px] mx-auto px-5 md:px-8 ">
-        <div className="grid gap-4">
+        <div className="grid gap-7">
           <div>
             <BreadcrumbSec />
           </div>
           <div>
             <Header />
+          </div>
+          <div>
+            <PhotoFrame />
           </div>
         </div>
       </div>
--- app/property-details/components/photo-frame/PhotoFrame.jsx
+++ app/property-details/components/photo-frame/PhotoFrame.jsx
@@ -1,3 +1,55 @@
+"use client";
+import img1 from "@/public/images/property_details/frame/img1.png";
+import img2 from "@/public/images/property_details/frame/img2.png";
+import img3 from "@/public/images/property_details/frame/img3.png";
+import img4 from "@/public/images/property_details/frame/img4.png";
+import img5 from "@/public/images/property_details/frame/img5.png";
+import link from "@/public/images/property_details/frame/link.png";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function PhotoFrame() {
-  return <div>PHoto frame</div>;
+  const [headerImg, setHeaderImg] = useState(img1);
+
+  const data = [
+    { src: img2, id: 1 },
+    { src: img3, id: 2 },
+    { src: img4, id: 3 },
+    { src: img5, id: 4 },
+  ];
+  const handleHeaderImg = (item) => {
+    setHeaderImg(item.src);
+  };
+  return (
+    <div>
+      <div>
+        <div>
+          <Image alt="" className="w-full rounded h-[560px]" src={headerImg} />
+        </div>
+        <div className="grid grid-cols-12 gap-7 py-7">
+          {data.map((item, index) => (
+            <div key={index} className="col-span-3 relative">
+              <button className="w-full" onClick={() => handleHeaderImg(item)}>
+                <Image src={item.src} alt="" className="w-full z-10" />
+              </button>
+              {data.length - 1 === index && (
+                <button className="absolute  top-[30%] left-[20%]  top-1 z-20">
+                  <Image
+                    alt=""
+                    onClick={() => {
+                      if (data.length - 1 === index) {
+                        alert("No more photo!");
+                      }
+                    }}
+                    src={link}
+                  />
+                </button>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
