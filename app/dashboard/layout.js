c5b7f5cefb845cf220862db268d0f3203924812d
msbhh-ms <learnwithmsbhh@gmail.com>
2024-02-26 13:17:07 +0600

    updated dashbaord container

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -5,7 +5,7 @@ import MobileNavbar from "./components/mobile_navbar/MobileNavbar";
 export default function RootLayout({ children }) {
   return (
     <div className="">
-      <div className="w-full max-w-[1630px] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] border-grayline mx-auto  ">
+      <div className="w-full max-w-[1630px] md:shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] border-grayline mx-auto  ">
         <div className="hidden md:grid md:grid-cols-12 lg:grid-cols-9 xl:grid-cols-12">
           <div className="md:col-span-3 lg:col-span-2 xl:col-span-2 ">
             <div className="relative">
